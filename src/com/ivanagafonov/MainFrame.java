ce4f52260f52aaac263e9df505dec5fe301d0f5a
Ivan <sudden1306@gmail.com>
2020-12-03 22:28:51 +0300

    Add buttons group
    Add buttons for the view. Not the best solution with BoxLayout, because the group is below the field, but not always at the bottom of the application. But the solution is simple.

--- src/com/ivanagafonov/MainFrame.java
+++ src/com/ivanagafonov/MainFrame.java
@@ -16,12 +16,23 @@ public class MainFrame extends JFrame {
         this.setExtendedState(Frame.MAXIMIZED_BOTH);
         this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
         this.setVisible(true);
+
+        buttons = new JPanel();
+        startButton = new JButton("Start");
+        stopButton = new JButton("Stop");
+        clearButton = new JButton("Clear");
+        buttons.add(startButton);
+        buttons.add(stopButton);
+        buttons.add(clearButton);
+        this.getContentPane().add(BorderLayout.SOUTH, buttons);
     }
+
+
 
     public static void main(String[] args) {
             EventQueue.invokeLater( () -> {
                 MainFrame mainFrame = new MainFrame();
-                mainFrame.getContentPane().add(BorderLayout.NORTH, new PlayingPanel(1, 1));
+                mainFrame.getContentPane().add(new PlayingPanel(1, 1), 0);
             });
     }
 }
