ab128ae027f012741ea1dbdb93ade609820999cb
Ivan <sudden1306@gmail.com>
2020-12-04 02:49:09 +0300

    Paint playing field
    Used a JPanel with rendering rectangle2D  because it is the least expensive option in memory. If each cell is represented as a JComponent, then it will be too heavy for the RAM.
    
    Introduced Cell, which is implementation dependent, will be fixed to abstract in the future.

--- src/com/ivanagafonov/PlayingPanel.java
+++ src/com/ivanagafonov/PlayingPanel.java
@@ -12,10 +12,10 @@ public class PlayingPanel extends JScrollPane {
         this.rows = rows;
         this.columns = columns;
 
-        setMaximumSize(new Dimension(rows * PlayingField.sideSize, columns * PlayingField.sideSize));
+        setMaximumSize(new Dimension(columns * PlayingField.sideSize + 5, rows * PlayingField.sideSize));
 
         field = new PlayingField();
-        field.setPreferredSize(new Dimension(rows * PlayingField.sideSize, columns * PlayingField.sideSize));
+        field.setPreferredSize(new Dimension(columns * PlayingField.sideSize, rows * PlayingField.sideSize));
         field.setBorder(BorderFactory.createLineBorder(Color.black));
 
         setViewportView(field);
@@ -27,7 +27,39 @@ public class PlayingPanel extends JScrollPane {
         @Override
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
+
+            float x = 0, y = 0;
+
+            Graphics2D g2 = (Graphics2D) g;
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    Cell cell = new Cell(x, x+sideSize, y,y+sideSize,j,i);
+                    Rectangle2D rect = new Rectangle2D.Float(x, y, sideSize, sideSize);
+                    g2.draw(rect);
+                    x += sideSize;
+                }
+                x = 0;
+                y += sideSize;
+            }
         }
+    }
+
+    class Cell {
+        private int column, row;
+        private float x1, x2, y1, y2;
+        private boolean isCaptured;
+
+        Cell(float x1, float x2, float y1, float y2, int column, int row) {
+            this.column = column;
+            this.row = row;
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+
+        public void capture(){}
+        public void release(){}
     }
 
 }
