7741ef1fc0145e5a9f9dbe208dd1a1b1b5cb25bf
Ivan <sudden1306@gmail.com>
2020-12-09 01:14:57 +0300

    Add tasks for threads death and life

--- src/com/ivanagafonov/GameLife.java
+++ src/com/ivanagafonov/GameLife.java
@@ -4,8 +4,10 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class GameLife {
-    List<List<Cell>> cells;
-    List<List<Cell>> last_cells;
+    private List<List<Cell>> cells;
+    private List<List<Cell>> last_cells;
+    private int countColumns;
+    private int countRows;
 
     public static void main(String[] argv) {
         GameLife gameLife = new GameLife();
@@ -23,6 +25,8 @@ public class GameLife {
 
     public  <T extends List<? extends List<? extends Cell>> & Cloneable> void play (T initState, int duration) {
         cells = new ArrayList<>((ArrayList<ArrayList<Cell>>) initState);
+        countColumns = cells.get(0).size();
+        countRows = cells.size();
 
         boolean isEmpty = true;
         for (int i = 0; i < cells.size(); i++) {
@@ -56,6 +60,91 @@ public class GameLife {
     }
 
     private void randomFill() {}
+
+    abstract class ChangeCell implements Callable<Boolean> {
+        private int row;
+        private int column;
+
+        ChangeCell(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getColumn() {
+            return column;
+        }
+
+        private int getCountNeighbors() {
+            int countNeighbors = 0;
+
+            if (getColumn() + 1 < countColumns &&
+                    last_cells.get(getRow()).get(getColumn() + 1).isCaptured())
+                countNeighbors++;
+            if (getRow() + 1 < countRows &&
+                    last_cells.get(getRow() + 1).get(getColumn()).isCaptured())
+                countNeighbors++;
+            if ( getRow() + 1 < countRows && getColumn() + 1 < countColumns &&
+                    last_cells.get(getRow() + 1).get(getColumn() + 1).isCaptured())
+                countNeighbors++;
+            if (getRow() > 0 &&
+                    last_cells.get(getRow() - 1).get(getColumn()).isCaptured())
+                countNeighbors++;
+            if (getColumn() > 0 &&
+                    last_cells.get(getRow()).get(getColumn() - 1).isCaptured())
+                countNeighbors++;
+            if (getRow() > 0 && getColumn() > 0 &&
+                    last_cells.get(getRow() - 1).get(getColumn() - 1).isCaptured())
+                countNeighbors++;
+            if (getRow() + 1 < countRows  && getColumn() > 0 &&
+                    last_cells.get(getRow() + 1).get(getColumn() - 1).isCaptured())
+                countNeighbors++;
+            if (getRow() > 0  && getColumn() + 1 < countColumns &&
+                    last_cells.get(getRow() - 1).get(getColumn() + 1).isCaptured())
+                countNeighbors++;
+
+            return countNeighbors;
+        }
+    }
+
+    class LifeCallable extends ChangeCell {
+
+        public static final int NEIGHBORS_FOR_NEW_LIFE = 3;
+
+        LifeCallable(int row, int column) {
+            super(row, column);
+        }
+
+        @Override
+        public Boolean call() throws Exception {
+            boolean newLife = super.getCountNeighbors() == NEIGHBORS_FOR_NEW_LIFE;
+
+            return newLife;
+        }
+    }
+
+    class DeathCallable extends ChangeCell {
+
+        public static final int NEIGHBORS_FOR_ALONE_DEATH = 2;
+        public static final int NEIGHBORS_FOR_FULLNESS_DEATH = 4;
+
+        DeathCallable(int row, int column) {
+            super(row, column);
+        }
+
+        @Override
+        public Boolean call() throws Exception {
+            int countNeighbors = super.getCountNeighbors();
+
+            boolean isDeath = countNeighbors < NEIGHBORS_FOR_ALONE_DEATH ||
+                    countNeighbors > NEIGHBORS_FOR_FULLNESS_DEATH;
+
+            return isDeath;
+        }
+    }
 }
 
 
