35b0f392f20a4b7791b2b4b3b6b1f39f3436145d
Ivan <sudden1306@gmail.com>
2020-12-14 20:42:47 +0300

    Replace waiting status threads to observer

---
+++ src/main/java/com/ivanagafonov/StatusEventManager.java
@@ -0,0 +1,40 @@
+package com.ivanagafonov;
+
+import java.util.ArrayList;
+import java.util.EventObject;
+import java.util.List;
+
+public class StatusEventManager {
+    List<StatusEventListener> listenerList = new ArrayList<>();
+
+    public void notify(StatusEvent event) {
+        listenerList.forEach((o) -> o.update(event.getStatus()));
+    }
+
+    public boolean subscribe(StatusEventListener listener) {
+        return listenerList.add(listener);
+    }
+
+    public boolean unsubscribe(StatusEventListener listener) {
+        return listenerList.remove(listener);
+    }
+}
+
+enum Status {RUNNING, STOPPED}
+
+interface StatusEventListener {
+    void update(Status status);
+}
+
+class StatusEvent extends EventObject {
+    private Status status;
+
+    public StatusEvent(Object source, Status status) {
+        super(source);
+        this.status = status;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+}
